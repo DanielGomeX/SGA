41aa44c418e156aa6a14a42dee5ca30c64faf311
eders <eders@DESKTOP-DELL-EDER>
2017-09-18 01:41:43 -0300

    Formulário de acesso ao sistema

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <h1 align="middle">Seja Bem vindo</h1>
+        
+        <h3 align="middle">Selecione seu usuário</h3>
+        
+        <form align="middle" name="login" action="conexaoBD.php" method="POST">
+            <strong><label>Usuário:</label></strong></br>
+            Nome: <input type="text" name='nomeusuario'/>
+            <select name="tipousuario">
+                <option value="">Você é?</option>
+                <option value="1">Administrador</option>
+                <option value="2">Balconista</option>
+                <option value="3">Professor</option>
+            </select></br></br>
+            Senha:  <input type="password" name="senha"/></br></br>
+            
+            <input type="submit" name="acessar" value="Acessar"/>
+        </form>
     </body>
 </html>
