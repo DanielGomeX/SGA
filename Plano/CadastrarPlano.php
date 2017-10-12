7edc87810ec382f04a81dad990c288091a49241f
Jeferson <Jeferson@192.168.13.140>
2017-10-12 18:21:00 -0300

    criação dos arquivos php "ConsultarPlano", "AlterarPlao", "ExcluirPlano"

--- Plano/CadastrarPlano.php
+++ Plano/CadastrarPlano.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Cadastro do Plano</title>
     </head>
     <body>
         <form action="CadastrarPlano.php" method="post">
-            <p>Informe o tipo de Plano:</p>
-            <input type="radio" name="plano" value="mensal"/> Mensal<br />
-            <input type="radio" name="plano" value="trimestral"/> Trimestral<br />
-            <input type="radio" name="plano" value="semestral"/> Semestral<br />
-            <input type="radio" name="plano" value="anual"/> Anual<br />
-            
-            <p>Informe a forma de Pagamento:</p>
-            <input type="radio" name="pagamento" value="dinheiro"/> Dinheiro<br />
-            <input type="radio" name="pagamento" value="cheque"/> Cheque<br />
-            <input type="radio" name="pagamento" value="cartao"/> Cartão<br />
-            
-            <p>Informe a Modalidade</p>
-            <input type="checkbox" name="opcoes" value="jiujitsu"/> JiuJitsu<br />
-            <input type="checkbox" name="opcoes" value="mma"/> MMA<br />
-            <input type="checkbox" name="opcoes" value="taekwondo"/> Taekwondo<br />
-            <input type="checkbox" name="opcoes" value="muaythai"/> Muay Thai<br />
-            
+            <fieldset>
+                <legend>Informe o tipo de Plano</legend>
+                    <input type="radio" name="plano" value="mensal"/> Mensal<br />
+                    <input type="radio" name="plano" value="trimestral"/> Trimestral<br />
+                    <input type="radio" name="plano" value="semestral"/> Semestral<br />
+                    <input type="radio" name="plano" value="anual"/> Anual<br />
+            </fieldset><br>
+            <fieldset>
+                <legend>Informe a forma de Pagamento</legend>
+                    <input type="radio" name="pagamento" value="dinheiro"/> Dinheiro<br />
+                    <input type="radio" name="pagamento" value="cheque"/> Cheque<br />
+                    <input type="radio" name="pagamento" value="cartao"/> Cartão<br />
+            </fieldset><br>
+            <fieldset>
+                <legend>Informe a Modalidade</legend>
+                    <input type="checkbox" name="opcoes" value="jiujitsu"/> JiuJitsu<br />
+                    <input type="checkbox" name="opcoes" value="mma"/> MMA<br />
+                    <input type="checkbox" name="opcoes" value="taekwondo"/> Taekwondo<br />
+                    <input type="checkbox" name="opcoes" value="muaythai"/> Muay Thai<br />
+            </fieldset><br>
             <input type="submit" name="salvar" value="Salvar">
             <input type="reset" name="limpar" value="Limpar">
             <input type="submit" name="voltar" value="Voltar">
+            
         </form>
         
     </body>
---
+++ Plano/ConsultarPlano.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Consulta do Plano</title>
+    </head>
+    <body>
+        <form>
+            <fieldset>
+                <legend>Selecione o Plano a ser consultado</legend>
+                <select name="plano">
+                    <option>JiuJitsu</option>
+                    <option>MMA</option>
+                    <option>Taekwondo</option>
+                    <option>Muay Tai</option>
+                </select>
+            </fieldset><br>
+            <input type="submit" name="consultar" value="Consultar">
+            <input type="submit" name="voltar" value="Voltar">
+        </form>
+    </body>
+</html>
